b6b72bb71be2aa3ec8c9d3a0a2d0e074d0e1f442
nvillanueva <nvillanueva@richrelevance.com>
2015-12-16 16:39:19 -0300

    oh look, i needed setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='poormanslogging',
+    version='1.0',
+    description='Dirt simple logging',
+    license='GPLv2+',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+    ],
+    author='Nicolas Villanueva',
+    author_email='villanueva.arg@gmail.com',
+    url='https://github.com/nicovillanueva/poormanslogging',
+    packages=['pyfiglet']
+)
