27ea566a1cc9fb5d0d6eb594cc91d54abd270647
nicovillanueva <villanueva.arg@gmail.com>
2016-04-15 16:15:41 -0300

    setup for publishing

--- setup.py
+++ setup.py
@@ -4,17 +4,20 @@ from setuptools import setup
 
 setup(
     name='poormanslogging',
+    packages=['poormanslogging'],
     version='1.0',
     description='Dirt simple logging',
+    long_description='A simple and straightforward library to log stuff into console. It uses colors and implements a spinner for long-running operations.',
+    author='Nicolas Villanueva',
+    author_email='villanueva.arg@gmail.com',
+    url='https://github.com/nicovillanueva/poormanslogging',
+    keywords='logging zeroconfig',
     license='GPLv2+',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Console',
+        'Programming Language :: Python :: 3',
         'Intended Audience :: Developers',
     ],
-    author='Nicolas Villanueva',
-    author_email='villanueva.arg@gmail.com',
-    url='https://github.com/nicovillanueva/poormanslogging',
-    packages=['poormanslogging'],
     install_requires=['colorama']
 )
